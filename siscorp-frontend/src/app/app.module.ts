dfda7afdb6ff38f8196e386aaba3edbcfd1eebb7
Igor Parra <igorricardoparra@gmail.com>
2022-06-08 21:16:19 -0300

    Criando tela de login

--- siscorp-frontend/src/app/app.module.ts
+++ siscorp-frontend/src/app/app.module.ts
@@ -5,8 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, LoginComponent],
@@ -16,6 +18,7 @@ import { HomeComponent } from './pages/home/home.component';
     BrowserAnimationsModule,
     MatCardModule,
     MatFormFieldModule,
+    MatInputModule, MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent],
